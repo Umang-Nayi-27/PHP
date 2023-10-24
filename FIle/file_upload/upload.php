09e794e4e2f2320c4062e1963e53450d411ecc03
Umang <105592320+Umang-Nayi-27@users.noreply.github.com>
2023-10-24 23:33:39 +0530

    Update upload.php

--- FIle/file_upload/upload.php
+++ FIle/file_upload/upload.php
@@ -23,5 +23,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
 <!-- 
 1. use $_file function and store file in temp...
-2. move file, which is in temp to...... its Main location usinng move_uploaded_file("temp","main") 
+2. move file, which is in temp to...... its Main location using move_uploaded_file("temp","main") 
 -->
+
+
+// MULTIPLE FILE UPLOAD
+/*
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_FILES["fileToUpload"])) {
+        $targetDirectory = "upload/";
+        
+        // Loop through the array of uploaded files
+        $count = count($_FILES["fileToUpload"]["name"]);
+        for ($i = 0; $i < $count; $i++) {
+            $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"][$i]);
+            
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $targetFile)) {
+                echo "The file " . $_FILES["fileToUpload"]["name"][$i] . " has been uploaded.<br>";
+            } else {
+                echo "Sorry, there was an error uploading your file " . $_FILES["fileToUpload"]["name"][$i] . ".<br>";
+            }
+        }
+    }
+}
+?>
+
+*/
